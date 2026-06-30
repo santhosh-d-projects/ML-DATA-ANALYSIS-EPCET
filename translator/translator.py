329a4ee3150255ea979ebbd58dcc716da145afbe
Santhosh D <santhoshsd364@gmail.com>
2026-06-30 22:20:45 +0530

    Remove translator files

--- translator/translator.py
+++
@@ -1,9 +0,0 @@
-from googletrans import Translator
-
-translator = Translator()
-
-text = input("Enter text in English: ")
-
-result = translator.translate(text, src="en", dest="hi")
-
-print("Translated Text:", result.text)
